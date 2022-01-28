588ea363c765894fa0d1548c41208c0a83e489d3
GlassB3ad <j.lahtivirta@gmail.com>
2022-01-28 17:50:21 +0200

    code seems to work, but needs more testing and cleaning

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class JSONparser {
     constructor () {}
-
+//---------------------------------------------------------------------------------------------------------------------------
+//This function takes javascript objects and turns them into proper JSON representations.    
     toJSON = (input) => {
         if(typeof(input) === 'object'){
             if(Array.isArray(input)){
@@ -9,11 +10,13 @@ class JSONparser {
                     if(typeof(input[i]) === 'string') {
                         result = result + '"' + input[i] + '"'
                     }
-                    if(typeof(input[i]) === 'object'){    
-                        result = result + this.toJSON(input[i])
-                    }
                     else {
-                        result = result + input[i]
+                        if(typeof(input[i]) === 'object'){    
+                            result = result + this.toJSON(input[i])
+                        }
+                        else {
+                            result = result + input[i]
+                        }
                     }
                     if(i != input.length-1){
                         result = result + ', '
@@ -49,28 +52,47 @@ class JSONparser {
         }
     }
 
+//----------------------------------------------------------------------------------------------------------------------------------------------    
+    //This function appears to convert proper JSON string into javascript object...
     fromJSON = (str) => {
-        const heap = []
         if(str[0] === '{'){
-            heap.push('{')
-            return (this.fromJSONHelper (str.slice(1), heap))[0]
+            return (this.fromJSONHelper (str.slice(1), '{'))[0]
         }
         if(str[0] === '['){
-            return (this.fromJSONHelper (str.slice(1), heap.push('[')))[0]
+            return (this.fromJSONHelper (str.slice(1), '['))[0]
         }
         throw new Error("Not JSON")
     }
-    fromJSONHelper (str, heap) {
-        if(heap[heap.length - 1] === '{'){
-            const entity = {}
-            let name = ""
-            let value = ""
-            let isValue = false
-            let isString = false
-            const addChar = (a) => {
-                if (isValue) value += a
-                else name += a
+
+    //... but this function does all the work
+    fromJSONHelper (str, char) {
+        //This function operates recursively and divides into two cases "object case" and "Array case". The case is defined by argument char. 
+        //Depending on the case, function returns either an object or an array. If given String is not proper JSON, function throws an error.
+        //helpfull variables used to gather values or to remember states of the parsing progress -->
+        let name = ""
+        let value = ""
+        let isValue = false
+        let isString = false
+        //Helper functions -->
+        const addChar = (a) => {
+            if (isValue) value += a
+            else name += a
+        }
+        const buildString = (x) => {
+            //for now, lets not worry about escaping " marks
+            if(x === '"'){
+                addChar('"')
+                isString = false
             }
+            else {
+                addChar(x)
+            }
+        }        
+        //Object case
+        if(char === '{'){
+            //return value ->
+            const entity = {}
+            //function adds new properties to entity
             const addProperty = () => {
                 if(!isValue) throw new Error("Not JSON") 
                 entity[name.slice(1,name.length-1)] = getValue(value)
@@ -78,11 +100,10 @@ class JSONparser {
                 value = ""
                 isValue = false
             }
-
+            //Code that runs the parsing process -->
             let i = 0
             for(i = 0; i < str.length; i++){
                 /*
-                console.log(heap)
                 console.log("String: " + str.slice(i))
                 console.log("isValue: " + isValue )
                 console.log("isString: " + isString )
@@ -90,16 +111,7 @@ class JSONparser {
                 console.log("value: " + value )
                 console.log("i: " + i)
                 */
-                if(isString){
-                    //for now, lets not worry about escaping " marks
-                    if(str[i] === '"'){
-                        addChar('"')
-                        isString = false
-                    }
-                    else {
-                        addChar(str[i])
-                    }
-                }
+                if(isString) buildString(str[i])
                 else{
                     if(str[i].search(/\s/) != -1){
                         //console.log("Whitespace found!")
@@ -116,7 +128,7 @@ class JSONparser {
                     if(str[i] === ']') throw new Error("Not JSON")
                     if(str[i] === '{' || str[i] === '['){
                         if(!isValue) throw new Error("Not JSON")
-                        const result = this.fromJSONHelper(str.slice(i+1), heap.concat(str[i]))
+                        const result = this.fromJSONHelper(str.slice(i+1), str[i])
                         i = i+result[1]
                         value = result[0]
                         continue
@@ -136,7 +148,51 @@ class JSONparser {
             //console.log([entity, i+1])
             return [entity, i+1]
         }
-        if(heap[heap.length - 1] === '['){    
+        //Array case
+        if(char === '['){
+            //Return value -->
+            const entity = []
+            //Function that add new itmes into array
+            const addItem = () => {
+                entity.push(getValue(value))
+                value = ""
+            }
+            //With arrays there is only values to be coded and no property names -->
+            isValue = true 
+            //Code that runs the parsing process -->
+            let i = 0
+            for(i = 0; i < str.length; i++){
+                if(isString) buildString(str[i])
+                else{
+                    //checks if current index contains whispace character and ignores it
+                    if(str[i].search(/\s/) != -1){
+                        //console.log("Whitespace found!")
+                        continue
+                    }
+                    if(str[i] === ']') {
+                        addItem()
+                        break
+                    }
+                    if(str[i] === '}') throw new Error("Not JSON")
+                    if(str[i] === '{' || str[i] === '['){
+                        const result = this.fromJSONHelper(str.slice(i+1), str[i])
+                        i = i+result[1]
+                        value = result[0]
+                        continue
+                    }
+                    if(str[i] === ','){
+                        addItem()
+                        continue
+                    }
+                    if(str[i] === '"'){
+                        addChar(str[i])
+                        isString = true
+                        continue
+                    }
+                    addChar(str[i])
+                }
+            }
+            return [entity, i+1]    
         }
     }
 }
--- tests.js
+++ tests.js
@@ -1,10 +1,14 @@
 const par = require('./index')
 
 
-const test2 = '{"ankka":"234","possu":12,"kissa":true,"kilpikonna":"karvainen","Emilia":{"Kissa2":100,"Kissa3": {"karvoitus":"tuuhea", "vihainen":true}},"JERE":{"KISSA":23,"KISSAA":34.3}}'
-console.log(par.toJSON(test1))
+const test2 = '{"ankka":"234","possu":12,"kissa":true,"kilpikonna":"karvainen","Emilia":{"Kissa2":100,"Kissa3": {"karvoitus":"tuuhea", "vihainen":true}},"JERE":{"KISSA":23,"KISSAA":34.3}, "taulukko":[234, 12, "KISSA"]}'
 const test2c = par.fromJSON(test2) 
 console.log(test2c)
 console.log(par.toJSON(test2c))
 console.log(par.fromJSON(par.toJSON(test2c)))
+const test3 = '["ankka","possu","aasi", [1000, 23, 43, true, 123.2345567]]'
+const test3res = par.fromJSON(test3) 
+console.log(test3res)
+console.log(par.toJSON(test3res))
+
 
